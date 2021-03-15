c14358cfeaf4ecb8713043a740c6b75705976317
Leon <hualiang.tian@gmail.com>
2021-03-14 21:57:03 -0400

    commit tree 0b474f725a8679dc2dc08631fda35ce4ba64066a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,26 @@ require 'pry'
 
 class String
 
+def intialize(phrase)
+  @phrase = phrase
+end
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    
+    self.split(/[\.?]|!{2}/).count
 
+    # binding.pry
   end
 end
